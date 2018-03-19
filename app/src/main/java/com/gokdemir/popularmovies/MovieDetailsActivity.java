3158b83d216147134366211984cf03fa9de6eeec
aligokdemir <denizaligkdmr@gmail.com>
2018-03-20 00:57:42 +0200

    Bottom navigation view added.

--- app/src/main/java/com/gokdemir/popularmovies/MovieDetailsActivity.java
+++ app/src/main/java/com/gokdemir/popularmovies/MovieDetailsActivity.java
@@ -2,18 +2,25 @@ package com.gokdemir.popularmovies;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
 import com.gokdemir.popularmovies.Model.MovieResults;
 import com.gokdemir.popularmovies.Utilities.NetworkUtils;
+
+import java.util.prefs.PreferenceChangeEvent;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -33,10 +40,13 @@ public class MovieDetailsActivity extends AppCompatActivity {
     public ImageView mBackdropImageHeader;
     @BindView(R.id.toolbar)
     public Toolbar toolbar;
+    @BindView(R.id.favMovie)
+    public FloatingActionButton mFloatingAction;
 
     private Context context;
 
     double voteAverage;
+    boolean isFavorite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +54,13 @@ public class MovieDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_movie_details);
         setSupportActionBar(toolbar);
 
-        MovieResults.Movie movie = getIntent().getParcelableExtra(getResources().getString(R.string.movie_key));
+        final MovieResults.Movie movie = getIntent().getParcelableExtra(getResources().
+                getString(R.string.movie_key));
+
+        SharedPreferences sharedPreferences = getSharedPreferences(getResources()
+                .getString(R.string.shared_pref_name), MODE_PRIVATE);
+        isFavorite = sharedPreferences.getBoolean(String.valueOf(movie.getId()),
+                Boolean.parseBoolean(null));
 
         CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.collapsingToolbar);
         collapsingToolbarLayout.setTitle(movie.getTitle());
@@ -56,14 +72,45 @@ public class MovieDetailsActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         mMovieTitle.setMovementMethod(new ScrollingMovementMethod());
 
-        voteAverage = movie.getVote_average();
+        if(isFavorite){
+            mFloatingAction.setImageResource(R.drawable.ic_favorite_black_24dp);
+        } else {
+            mFloatingAction.setImageResource(R.drawable.ic_favorite_border_black_24dp);
+        }
 
+        voteAverage = movie.getVote_average();
         mMovieTitle.setText(movie.getTitle());
         NetworkUtils.loadImageURL(movie.getPoster_path(), mMoviePoster);
         NetworkUtils.loadBackdropURL(movie.getBackdrop_path(), mBackdropImageHeader);
         mMovieReleaseDate.setText(movie.getRelease_date());
         mMovieVoteAverage.setText(voteAverage + "/10");
         mMovieOverview.setText(movie.getOverview());
+
+        mFloatingAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isFavorite){
+                    mFloatingAction.setImageResource(R.drawable.ic_favorite_border_black_24dp);
+                    SharedPreferences.Editor editor = getSharedPreferences(getResources().
+                            getString(R.string.shared_pref_name), MODE_PRIVATE).edit();
+                    editor.remove(String.valueOf(movie.getId()));
+                    editor.apply();
+                    Toast.makeText(context, "The movie is removed from favorites!",
+                            Toast.LENGTH_SHORT).show();
+                    //remove the movie from database...
+
+                } else{
+                    mFloatingAction.setImageResource(R.drawable.ic_favorite_black_24dp);
+                    SharedPreferences.Editor editor = getSharedPreferences(getResources().
+                            getString(R.string.shared_pref_name), MODE_PRIVATE).edit();
+                    editor.putBoolean(String.valueOf(movie.getId()), true);
+                    editor.apply();
+                    Toast.makeText(context, "The movie is added to favorites!",
+                            Toast.LENGTH_SHORT).show();
+                    //add the movie to the database...
+                }
+            }
+        });
     }
 
     @Override
@@ -74,5 +121,11 @@ public class MovieDetailsActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp(){
+        onBackPressed();
+        return true;
     }
 }
